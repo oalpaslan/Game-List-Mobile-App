22643f44b8a6a85b70e765367695db235e7ff319
oalpaslan <oalpaslan88@gmail.com>
2022-08-15 14:32:15 +0300

    some packages

--- Game List Mobile App/ListApp/source/screens/UserLogin.js
+++ Game List Mobile App/ListApp/source/screens/UserLogin.js
@@ -3,21 +3,28 @@ import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 
 const UserLogin = ({ navigation }) => {
   const [username, setUsername] = useState("");
-
+  const [isValid, setIsValid] = useState(false);
   return (
     <View style={styles.viewStyle}>
       <Text style={styles.titleStyle}>Your name:</Text>
+
       <TextInput
-        style={styles.inputStyle}
+        style={isValid ? styles.validInputStyle : styles.invalidInputStyle}
         value={username}
-        onChangeText={setUsername}
-      />
+        onChangeText={(username) => {
+          username === "/^[a-z0-9]+$/i"
+            ? setIsValid(true) && setUsername(username)
+            : setIsValid(false);
+        }}
+      ></TextInput>
       <Button
         title="Continue"
         onPress={({}) =>
-          navigation.navigate("Home", {
-            username: username,
-          })
+          username === ""
+            ? setIsValid(false)
+            : navigation.navigate("Home", {
+                username: username,
+              })
         }
       />
     </View>
@@ -34,10 +41,21 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     height: "100%",
     justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
   },
-  inputStyle: {
+  validInputStyle: {
     backgroundColor: "white",
+    borderBottomColor: "green",
     alignSelf: "center",
+    width: "60%",
+    padding: 5,
+    margin: 10,
+  },
+  invalidInputStyle: {
+    backgroundColor: "red",
+    alignSelf: "center",
+    borderBottomColor: "red",
     width: "60%",
     padding: 5,
     margin: 10,
